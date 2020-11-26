b464c2888e76ccc6c32af2814424ac1a33b77e8c
sabrexa <47918559+sabrexa@users.noreply.github.com>
2020-11-26 18:18:35 +0100

    Update compress_videos.py

--- preprocessing/compress_videos.py
+++ preprocessing/compress_videos.py
@@ -4,21 +4,41 @@ import argparse
 import os
 #contains a variety of things to do with random number generation
 import random
-#
+# provides a consistent interface to creating and working with additional processes | 
 import subprocess
 
+#tto limit the number of threads used to the number of cpus I demand
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
 os.environ["OMP_NUM_THREADS"] = "1"
+#create partial functions in python by using the partial function
+#Partial functions allow one to derive a function with x parameters to a function 
+#with fewer parameters and fixed values set for the more limited function.
+#Example :
+#def func(u,v,w,x):
+#    return u*4 + v*3 + w*2 + x
+
+#p = partial(func,5,6,7) we define u = 5, v = 6, w = 7
+#print(p(8)) x = 8
 from functools import partial
+#finds all the pathnames matching a specified pattern according to the rules used by the Unix shell
 from glob import glob
+#used for parallel execution of a function across multiple input values, distributing the input data across processes (data parallelism)
 from multiprocessing.pool import Pool
+#get the number of cpu's in the system
 from os import cpu_count
 
+#library using which we can develop real-time computer vision applications.
+#It mainly focuses on image processing, video capture and analysis including features like face detection and object detection.
 import cv2
 
 cv2.ocl.setUseOpenCL(False)
+
+#OpenCV will disable threading optimizations and run all it's functions sequentially
+#(to avoid strange crashes related to opencv)
 cv2.setNumThreads(0)
+
+# to make your loops show a progress meter
 from tqdm import tqdm
 
 
@@ -45,5 +65,7 @@ if __name__ == '__main__':
     videos = [video_path for video_path in glob(os.path.join(args.root_dir, "*/*.mp4"))]
     with Pool(processes=cpu_count() - 2) as p:
         with tqdm(total=len(videos)) as pbar:
+            # imap_unordered: this method chops the iterable into a number of chunks which it submits to the process pool as separate tasks.
+            # the ordering of the results from the returned iterator are considered arbitrary
             for v in p.imap_unordered(partial(compress_video, root_dir=args.root_dir), videos):
                 pbar.update()
